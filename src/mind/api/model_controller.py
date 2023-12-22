72303d5558f87ab3299671ff89bc6aacfb9bdbb0
enaut-genua <enaut.genua@alumni.mondragon.edu>
2023-12-22 20:36:34 +0100

    Add a package

---
+++ src/mind/api/model_controller.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, File, Response, UploadFile, status
+
+
+class ModelController:
+    __app: FastAPI = FastAPI()
+
+    @__app.post("/predict")
+    async def predict(self, uploaded_img: UploadFile = File(...)) -> Response:
+        content: bytes = await uploaded_img.read() # This content will be sent to the model.
+        # prediction: bytes = predict()
+        # return StreamingResponse(content, media_type="image/png")
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
