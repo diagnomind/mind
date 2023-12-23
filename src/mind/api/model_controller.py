ac55bcea8bdd3dff1dd7832257ce5369746732f2
enaut-genua <enaut.genua@alumni.mondragon.edu>
2023-12-23 13:21:07 +0100

    Clean and make a controller skeleton, with tests enabled

--- src/mind/api/model_controller.py
+++ src/mind/api/model_controller.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, Response, UploadFile, status
+from fastapi import FastAPI, File, Response, UploadFile
 
 
 class ModelController:
@@ -6,8 +6,8 @@ class ModelController:
 
     @__app.post("/predict")
     async def predict(self, uploaded_img: UploadFile = File(...)) -> Response:
-        content: bytes = await uploaded_img.read() # This content will be sent to the model.
-        # prediction: bytes = predict()
-        # return StreamingResponse(content, media_type="image/png")
         raise NotImplementedError()
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+    @__app.post("/train")
+    async def train(self) -> Response:
+        raise NotImplementedError()
---
+++ tests/api/model_controller_test.py
@@ -0,0 +1,6 @@
+class TestModelController:
+    def test_predict(self):
+        raise NotImplementedError()
+    
+    def test_train(self):
+        raise NotImplementedError()
