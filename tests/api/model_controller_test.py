5ebad48ed36dc55be5e563e28e6d900afc169cb1
enaut-genua <enaut.genua@alumni.mondragon.edu>
2023-12-27 00:49:17 +0100

    Revert changes in tests, and change job name

--- tests/api/model_controller_test.py
+++ tests/api/model_controller_test.py
@@ -1,8 +1,6 @@
 class TestModelController:
     def test_predict(self):
-        # raise NotImplementedError()
-        assert 1 != 2
+        raise NotImplementedError()
 
     def test_train(self):
-        # raise NotImplementedError()
-        assert 1 != 2
+        raise NotImplementedError()
