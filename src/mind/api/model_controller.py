c5934077bd46cf0225ba97043ddde0a4de645607
gaizki3002 <gaizka.saenzdesamani@alumni.mondragon.edu>
2024-01-18 14:04:03 +0100

    endpoint_simulation

--- src/mind/api/model_controller.py
+++ src/mind/api/model_controller.py
@@ -19,3 +19,16 @@ async def predict(uploaded_img: UploadFile = File(...)) -> Response:
         return Response(content=encoded_image.tobytes(), media_type="image/png", headers=headers)
     except Exception as e:
         return Response(content=f"Error: {str(e)}", status_code=500)
+    
+@_app.post("/simulation")
+async def simulation(uploaded_img: UploadFile = File(...)) -> Response:
+    try:
+        raw_img = cv2.imdecode(np.frombuffer(await uploaded_img.read(), np.uint8), cv2.IMREAD_COLOR)
+        _, encoded_image = cv2.imencode(".png", raw_img)
+        headers = {
+            "Content-Type": "image/png",
+            "Content-Disposition": "inline; filename=prediction.png",
+        }
+        return Response(content=encoded_image.tobytes(), media_type="image/png", headers=headers)
+    except Exception as e:
+        return Response(content=f"Error: {str(e)}", status_code=500)
