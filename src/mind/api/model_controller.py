b1dd780c92f2657cbfb3cfecb05e3bbe95bf5596
gaizki3002 <gaizka.saenzdesamani@alumni.mondragon.edu>
2024-01-20 16:38:51 +0100

    Refactor

--- src/mind/api/model_controller.py
+++ src/mind/api/model_controller.py
@@ -1,7 +1,10 @@
+import time
 import cv2
 from fastapi import FastAPI, File, Response, UploadFile
 import numpy as np
 from mind.model.predictor import Predictor
+
+CONTENT_TYPE = "image/png"
 
 _app = FastAPI()
 _predictor = Predictor()
@@ -13,22 +16,22 @@ async def predict(uploaded_img: UploadFile = File(...)) -> Response:
         predicted =  _predictor.predict(raw_img, 0.01)
         _, encoded_image = cv2.imencode(".png", predicted)
         headers = {
-            "Content-Type": "image/png",
+            "Content-Type": CONTENT_TYPE,
             "Content-Disposition": "inline; filename=prediction.png",
         }
-        return Response(content=encoded_image.tobytes(), media_type="image/png", headers=headers)
+        return Response(content=encoded_image.tobytes(), media_type=CONTENT_TYPE, headers=headers)
     except Exception as e:
         return Response(content=f"Error: {str(e)}", status_code=500)
     
-@_app.post("/simulation")
-async def simulation(uploaded_img: UploadFile = File(...)) -> Response:
+@_app.get("/simulation")
+async def simulation() -> Response:
     try:
-        raw_img = cv2.imdecode(np.frombuffer(await uploaded_img.read(), np.uint8), cv2.IMREAD_COLOR)
-        _, encoded_image = cv2.imencode(".png", raw_img)
+        noise_image = np.random.default_rng(int(time.time())).integers(0, 256, size=(256, 256, 3), dtype=np.uint8)
+        _ , encoded_image = cv2.imencode(".png", noise_image)
         headers = {
-            "Content-Type": "image/png",
+            "Content-Type": CONTENT_TYPE,
             "Content-Disposition": "inline; filename=prediction.png",
         }
-        return Response(content=encoded_image.tobytes(), media_type="image/png", headers=headers)
+        return Response(content=encoded_image.tobytes(), media_type=CONTENT_TYPE, headers=headers)
     except Exception as e:
         return Response(content=f"Error: {str(e)}", status_code=500)
